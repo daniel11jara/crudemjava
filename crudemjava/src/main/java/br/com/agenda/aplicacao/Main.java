4f24cf33a3246facc907a234bf5728d38ec60db0
Daniel Jara <daniel11jara@gmail.com>
2023-12-16 16:41:53 -0300

    aula 05

--- crudemjava/src/main/java/br/com/agenda/aplicacao/Main.java
+++ crudemjava/src/main/java/br/com/agenda/aplicacao/Main.java
@@ -2,24 +2,34 @@ package br.com.agenda.aplicacao;
 
 
 
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.Iterator;
 
 import br.com.agenda.dao.ContatoDAO;
 import br.com.agenda.model.Contato;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		
 		ContatoDAO contatoDao = new ContatoDAO();
 		
 		Contato contato = new Contato();
-		contato.setNome("Maria");
-		contato.setIdade(66);
+		contato.setNome("Renan");
+		contato.setIdade(89);
 		
 		
 		contatoDao.save(contato);
+		
+		//aula 05 - visualizacao dos dados
+		
+		for (Contato c : contatoDao.getContatos()) {
+			
+			System.out.println("Contatos: " + c.getNome());
+			
+		}
 
 	}
 
